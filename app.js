0822f04a51ab4f73df3e035e5f5cf72de22bf081
Florent <florent.foure@gmail.com>
2017-10-14 21:31:41 +0200

    some changes for a JSON lookalike API

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 var express = require('express');
 var app = express();
 
-var template = ' \
-<!DOCTYPE html> <html> <body> \
-	<script type="text/javascript"> \
-		    var source = new EventSource("/events/"); \
-		    source.onmessage = function(e) { \
-		        document.body.innerHTML += e.data + "<br>"; \
-		    }; \
-</script> </body> </html>';
+var id = 1;
 
-app.get('/', function(req, res) {
-	res.send(template);  // <- Return the static template above
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 var clientId = 0;
@@ -19,11 +14,11 @@ var clients = {};  // <- Keep a map of attached clients
 
 // Called once for each new client. Note, this response is left open!
 app.get('/events/', function(req, res) {
-	req.socket.setTimeout(Infinity);
+	req.socket.setTimeout(Number.MAX_VALUE);
     res.writeHead(200, {
     	'Content-Type': 'text/event-stream',  // <- Important headers
     	'Cache-Control': 'no-cache',
-    	'Connection': 'keep-alive'
+		'Connection': 'keep-alive'
     });
     res.write('\n');
     (function(clientId) {
@@ -31,13 +26,34 @@ app.get('/events/', function(req, res) {
         req.on("close", function(){delete clients[clientId]});  // <- Remove this client when he disconnects
     })(++clientId)
 });
+app.get('/existingevents', function(req, res) {
+	var msgs = [];
+	res.setHeader('Content-Type', 'application/json');
+	for (var i = 0; i < 50; i++) {
+		msgs.push({
+			id: 10000 + i,
+			dataAttr: Math.random(),
+			label: 'test',
+			date: new Date()
+		});
+	}
+	res.send(JSON.stringify({ 'dummies' : msgs }));
+});
+
 
 setInterval(function(){
-	var msg = Math.random();
+	var msg = JSON.stringify({
+		id: id,
+		dataAttr: Math.random(),
+		label: 'test',
+		date: new Date()
+	});
+	++id;
+	
 	console.log("Clients: " + Object.keys(clients) + " <- " + msg);
 	for (clientId in clients) {
 		clients[clientId].write("data: "+ msg + "\n\n"); // <- Push a message to a single attached client
 	};
 }, 2000);
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8181);
